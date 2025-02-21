cf3272e9c8e98486968f22a493ed469016b9b6dd
aziza20-ux <s.afadhali@alustudent.com>
2025-02-21 15:03:29 +0200

    the new commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+this module contains a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit
 """
 
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
+    function queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
-        print("OK")
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    h = {'User-agent': 'ad-egg'}
+    r = requests.get(
+            url, headers=h, allow_redirects=False, params={'limit': 10})
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("OK")
+    if r.status_code == 200:
+        req = r.json()
+        data = req.get('data')
+        children = data.get('children')
+        if data is None or children is None:
+            print('None')
+        else:
+            for post in children:
+                post_data = post.get('data')
+                title = post_data.get('title')
+                print(title)
+    else:
+        print('None')
